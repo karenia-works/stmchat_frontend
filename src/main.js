425f3b5be382575856a8b65e0c127a9721918608
skuld <sywang1207@gmail.com>
2020-05-31 23:21:46 +0800

    generate vue project

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./plugins/element.js";
+import { Button } from "element-ui";
+
+Vue.config.productionTip = false;
+Vue.use(Button);
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
