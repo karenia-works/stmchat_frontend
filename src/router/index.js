8210a04ff30d7a5ef862c2a1c2210502fb61d8aa
skuld <sywang1207@gmail.com>
2020-06-04 12:40:00 +0800

    restore router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import Router from "vue-router";
+import VueRouter from "vue-router";
 import Login from "@/components/Login";
 import Regist from "@/components/Register";
 import Addmember from "@/components/Addmember";
@@ -7,38 +7,44 @@ import Setgroup from "@/components/Setgroup";
 import Contacts from "@/components/Contacts";
 import Chat from "@/components/Chat";
 
-Vue.use(Router);
+Vue.use(VueRouter);
 
-export default new Router({
-  routes: [
-    {
-      path: "/Login",
-      name: "Login",
-      component: Login,
-    },
-    {
-      path: "/Regist",
-      name: "Regist",
-      component: Regist,
-    },
-    {
-      path: "/Addmember",
-      name: "Addmember",
-      component: Addmember,
-    },
-    {
-      path: "/Setgroup",
-      name: "Setgroup",
-      component: Setgroup,
-    },
-    {
-      path: "/Contacts",
-      name: "Contacts",
-      component: Contacts,
-    },
-    {
-      path: "/chat",
-      component: Chat,
-    },
-  ],
+const routes = [
+  {
+    path: "/Login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/Regist",
+    name: "Regist",
+    component: Regist,
+  },
+  {
+    path: "/Addmember",
+    name: "Addmember",
+    component: Addmember,
+  },
+  {
+    path: "/Setgroup",
+    name: "Setgroup",
+    component: Setgroup,
+  },
+  {
+    path: "/Contacts",
+    name: "Contacts",
+    component: Contacts,
+  },
+  {
+    path: "/chat",
+    component: Chat,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
 });
+
+export default router;
