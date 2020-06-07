95aaa20809b9c71cf2e3dfbef1a29f559ab80db9
“MaksimXing <henryx@buaa.edu.cn>
2020-06-07 11:18:31 +0800

    login darkmode

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,83 +1,117 @@
 <template>
   <el-container style="padding:10px;height: 755px; border: 1px solid #eee">
-    <el-header>STM-Chat---STM for Lighter Communication</el-header>
-    <el-main>
+    <el-header class="dark_light_bg dark_main_text">STM-Chat---STM for Lighter Communication</el-header>
+    <el-main class="dark_deep_bg">
       <el-row>
-        <el-col :span="6"><div class="grid-content"></div></el-col>
+        <el-col :span="6">
+          <div class="grid-content"></div>
+        </el-col>
         <el-col :span="12">
-          <el-card class="box-card" shadow="always">
-            <div slot="header">
+          <el-card class="box-card dark_midium_bg" shadow="always">
+            <div slot="header" class="dark_main_text">
               Welcome to STM-Chat
               <el-link
                 :underline="false"
                 style="float:right"
                 v-on:click="ToRegister"
-                >暂无账户？马上注册</el-link
-              >
+                class="dark_main_text"
+              >暂无账户？马上注册</el-link>
             </div>
             <el-row>
-              <el-col :span="5"><div class="grid-content"></div></el-col>
-              <el-col :span="12"
-                ><div class="demo-input-suffix">
+              <el-col :span="5">
+                <div class="grid-content"></div>
+              </el-col>
+              <el-col :span="12">
+                <div class="demo-input-suffix">
                   <el-input
                     placeholder="用户ID/手机号"
                     prefix-icon="el-icon-user-solid"
                     clearable
                     v-model="userID"
-                  >
-                  </el-input>
+                  ></el-input>
 
                   <el-input
                     placeholder="密码"
                     prefix-icon="el-icon-key"
                     v-model="userPasswd"
                     show-password
-                  >
-                  </el-input>
+                  ></el-input>
 
-                  <el-button type="primary" v-on:click="login">登录</el-button>
-                </div></el-col
-              >
-              <el-col :span="6"><div class="grid-content"></div></el-col>
+                  <el-button type="primary" v-on:click="login" class="dark_light_bg">登录</el-button>
+                </div>
+              </el-col>
+              <el-col :span="6">
+                <div class="grid-content"></div>
+              </el-col>
             </el-row>
           </el-card>
         </el-col>
-        <el-col :span="6"><div class="grid-content"></div></el-col>
+        <el-col :span="6">
+          <div class="grid-content"></div>
+        </el-col>
       </el-row>
     </el-main>
   </el-container>
 </template>
 
 <style lang="stylus" scoped>
-
-.el-header,
-.el-footer {
+.el-header, .el-footer {
   background-color: #5edaa2;
   color: #333;
   text-align: left;
   line-height: 60px;
 }
+
 .el-main {
   background-color: #a9f1e8;
   color: #333;
   text-align: center;
   line-height: 70px;
 }
+
 .box-card {
   align: center;
 }
+
 .el-row {
   margin-bottom: 20px;
+
   &:last-child {
     margin-bottom: 0;
   }
 }
+
 .el-col {
   border-radius: 4px;
 }
+
 .grid-content {
   border-radius: 4px;
   min-height: 36px;
+}
+
+@media (prefers-color-scheme: dark) {
+  .dark_light_bg {
+    background-color: #606266;
+    border-color: #44474E;
+  }
+
+  .dark_midium_bg {
+    background-color: #44474E;
+    border-color: #606266;
+  }
+
+  .dark_deep_bg {
+    background-color: #303133;
+  }
+
+  .dark_sub_text {
+    color: #909399;
+  }
+
+  .dark_main_text {
+    color: #E4E7ED;
+  }
 }
 </style>
 
