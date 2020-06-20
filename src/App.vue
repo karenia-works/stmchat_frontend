a7c6713cb9fb1b831c4455f92864c40046efb24d
Rynco Maekawa <lynzrand@outlook.com>
2020-06-20 22:15:31 +0800

    Register di at start of app

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,14 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "./components/HelloWorld.vue";
-
+import { serviceProvider } from "./services/dependencyInjection";
+import registerDI from "./services/registerDI";
 @Component({
   components: {
     HelloWorld,
+  },
+  beforeCreate() {
+    registerDI();
   },
 })
 export default class App extends Vue {}
@@ -18,9 +22,9 @@ export default class App extends Vue {}
 
 <style lang="stylus">
 #app {
-  font-family: 'PingFang SC', 'Microsoft YaHei', 'San Francisco UI', 'Helvetica Neue', Arial;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: 'PingFang SC', 'Microsoft YaHei', 'San Francisco UI', 'Helvetica Neue', Arial
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
   // text-align: center
   // color: #2c3e50
   // margin-top: 60px
--- src/services/registerDI.ts
+++ src/services/registerDI.ts
@@ -6,49 +6,54 @@ import { UserProfilePool, GroupProfilePool } from "./cachingService";
 import { LoginService } from "./loginService";
 import { ChatMessageService, MessageListService } from "./messageService";
 
-container.register<IServerConfig>(TYPES.ServerConfig, {
-  useValue: serverConfig,
-});
-container.register<WsMessageService>(TYPES.WsMessageService, {
-  useToken: WsMessageService,
-});
-container.register<WsMessageService>(WsMessageService, {
-  useClass: WsMessageService,
-});
-container.register<UserProfilePool>(TYPES.UserProfilePool, {
-  useToken: UserProfilePool,
-});
-container.register<UserProfilePool>(UserProfilePool, {
-  useClass: UserProfilePool,
-});
-container.register<GroupProfilePool>(TYPES.GroupProfilePool, {
-  useToken: GroupProfilePool,
-});
-container.register<GroupProfilePool>(GroupProfilePool, {
-  useClass: GroupProfilePool,
-});
-container.register(TYPES.types, { useValue: TYPES });
-// container.register<ProfilePool<GroupProfile>>(GroupProfilePool, {
-//   useToken: GroupProfilePool,
-// });
-// container.register<ProfilePool<GroupProfile>>(GroupProfilePool, {
-//   useToken: GroupProfilePool,
-// });
-container.register<LoginService>(LoginService, { useClass: LoginService });
-container.register<LoginService>(TYPES.LoginService, {
-  useToken: LoginService,
-});
-container.register<ChatMessageService>(ChatMessageService, {
-  useClass: ChatMessageService,
-});
-container.register<ChatMessageService>(TYPES.ChatMessageService, {
-  useToken: ChatMessageService,
-});
-container.register<MessageListService>(MessageListService, {
-  useClass: MessageListService,
-});
-container.register<MessageListService>(TYPES.MessageListService, {
-  useToken: MessageListService,
-});
+var registered = false;
+function registerAll() {
+  if (registered) return;
+  registered = true;
+  container.register<IServerConfig>(TYPES.ServerConfig, {
+    useValue: serverConfig,
+  });
+  container.register<WsMessageService>(TYPES.WsMessageService, {
+    useToken: WsMessageService,
+  });
+  container.register<WsMessageService>(WsMessageService, {
+    useClass: WsMessageService,
+  });
+  container.register<UserProfilePool>(TYPES.UserProfilePool, {
+    useToken: UserProfilePool,
+  });
+  container.register<UserProfilePool>(UserProfilePool, {
+    useClass: UserProfilePool,
+  });
+  container.register<GroupProfilePool>(TYPES.GroupProfilePool, {
+    useToken: GroupProfilePool,
+  });
+  container.register<GroupProfilePool>(GroupProfilePool, {
+    useClass: GroupProfilePool,
+  });
+  container.register(TYPES.types, { useValue: TYPES });
+  // container.register<ProfilePool<GroupProfile>>(GroupProfilePool, {
+  //   useToken: GroupProfilePool,
+  // });
+  // container.register<ProfilePool<GroupProfile>>(GroupProfilePool, {
+  //   useToken: GroupProfilePool,
+  // });
+  container.register<LoginService>(LoginService, { useClass: LoginService });
+  container.register<LoginService>(TYPES.LoginService, {
+    useToken: LoginService,
+  });
+  container.register<ChatMessageService>(ChatMessageService, {
+    useClass: ChatMessageService,
+  });
+  container.register<ChatMessageService>(TYPES.ChatMessageService, {
+    useToken: ChatMessageService,
+  });
+  container.register<MessageListService>(MessageListService, {
+    useClass: MessageListService,
+  });
+  container.register<MessageListService>(TYPES.MessageListService, {
+    useToken: MessageListService,
+  });
+}
 
-export default "registered";
+export default registerAll;
