5eb1b49a6ef2a6e570cb923a9e36e314836b53df
zyn2github <48922540+zyn2github@users.noreply.github.com>
2020-06-23 00:50:08 +0800

    debug

--- src/services/serverConfig.ts
+++ src/services/serverConfig.ts
@@ -71,7 +71,7 @@ if (environment == "development") {
   _cfg.apiBaseUrl = "http://yuuna.srv.karenia.cc/api/v1";
   _cfg.getFile = "http://yuuna.srv.karenia.cc{name}";
   _cfg.auth.tokenEndpoint = "http://yuuna.srv.karenia.cc/connect/token";
-  _cfg.debug = { wsEndpointOverride: "ws://yuuna.srv.karenia.cc/ws/li" };
+  _cfg.debug = { wsEndpointOverride: "ws://yuuna.srv.karenia.cc/ws/yang" };
 }
 
 export const serverConfig: IServerConfig = _cfg;
